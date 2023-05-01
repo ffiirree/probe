ee6c178eeeb2cb6e370ac84ede1379f2f6736da6
ffiirree <ice_qi@163.com>
2023-05-01 17:59:29 +0800

    [linux/graphics] get utf8 window title

--- src/graphics/display_linux.cpp
+++ src/graphics/display_linux.cpp
@@ -3,6 +3,7 @@
 #include "probe/defer.h"
 #include "probe/graphics.h"
 
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
 
@@ -24,11 +25,19 @@ namespace probe::graphics
         return 0;
     }
 
-    // A slightly unusual feature is that a display is defined as a workstation consisting
-    // of a keyboard, a pointing device such as a mouse, and one or more screens.
-    // Multiple screens can work together, with mouse movement allowed to cross physical
-    // screen boundaries. As long as multiple screens are controlled by a single user with
-    // a single keyboard and pointing device, they comprise only a single display.
+    // The X Window System supports one or more screens containing overlapping windows or subwindows. A
+    // screen is a physical monitor and hardware, which can be either color, grayscale, or monochrome. There
+    // can be multiple screens for each display or workstation. A single X server can provide display
+    // services for any number of screens. A set of screens for a single user with one keyboard and one
+    // pointer (usually a mouse) is called a display.
+    //
+    // A slightly unusual feature is that a display is defined as a workstation consisting of a keyboard, a
+    // pointing device such as a mouse, and one or more screens. Multiple screens can work together, with
+    // mouse movement allowed to cross physical screen boundaries. As long as multiple screens are
+    // controlled by a single user with a single keyboard and pointing device, they comprise only a single
+    // display.
+    //
+    // ATTENTION: our 'display' concept is equal to the 'screen' concept in X.
     std::vector<display_t> displays()
     {
         std::vector<display_t> _displays;
@@ -77,12 +86,18 @@ namespace probe::graphics
         }
         return _displays;
     }
-
+    //
+    // All the windows in an X server are arranged in strict hierarchies. At the top of each hierarchy is a
+    // root window, which covers each of the display screens. Each root window is partially or completely
+    // covered by child windows. All windows, except for root windows, have parents. There is usually at
+    // least one window for each application program. Child windows may in turn have their own children. In
+    // this way, an application program can create an arbitrarily deep tree on each screen. X provides
+    // graphics, text, and raster operations for windows.
     std::deque<window_t> windows(bool visible)
     {
         std::deque<window_t> ret;
 
-        auto display = XOpenDisplay(nullptr);
+        auto display = ::XOpenDisplay(nullptr);
         if (!display) return {};
 
         auto root_window = DefaultRootWindow(display);
@@ -95,31 +110,58 @@ namespace probe::graphics
         // the specified window. The children are listed in current stacking order, from bottommost (first)
         // to topmost (last). XQueryTree() returns zero if it fails and nonzero if it succeeds. To free a
         // non-NULL children list when it is no longer needed, use XFree().
-        XQueryTree(display, root_window, &root_return, &parent_return, &child_windows, &child_num);
+        ::XQueryTree(display, root_window, &root_return, &parent_return, &child_windows, &child_num);
 
         for (unsigned int i = 0; i < child_num; ++i) {
             XWindowAttributes attrs{};
 
-            XGetWindowAttributes(display, child_windows[i], &attrs);
+            ::XGetWindowAttributes(display, child_windows[i], &attrs);
 
             if (visible && (attrs.map_state < 2 || (attrs.width * attrs.height < 4))) {
                 continue;
             }
 
-            char *buffer = nullptr;
-            XFetchName(display, child_windows[i], &buffer);
-            defer(XFree(buffer));
+            // title
+            std::string title{};
+            char *name = nullptr;
+            if (::XFetchName(display, child_windows[i], &name) > 0 && name != nullptr) {
+                title = name;
+                ::XFree(name);
+            }
+            else {
+                XTextProperty wmname{};
+                if (::XGetWMName(display, child_windows[i], &wmname) != 0 && wmname.value &&
+                    wmname.format == 8) {
+                    char **list = nullptr;
+                    int num     = 0;
+                    // COMPOUND_TEXT, STRING, UTF8_STRING or the encoding of the current locale is
+                    // guaranteed
+                    if (::Xutf8TextPropertyToTextList(display, &wmname, &list, &num) >= 0 && num > 0 &&
+                        *list) {
+                        title = *list;
+                        ::XFreeStringList(list);
+                    }
+                }
+                ::XFree(wmname.value);
+            }
 
-            ret.push_front({ (buffer ? buffer : std::string{}),
-                             {},
-                             geometry_t{ attrs.x, attrs.y, static_cast<uint32_t>(attrs.width),
-                                         static_cast<uint32_t>(attrs.height) },
-                             static_cast<uint64_t>(child_windows[i]),
-                             attrs.map_state >= 2 });
+            ret.push_front({
+                .name      = title,
+                .classname = {},
+                .rect =
+                    geometry_t{
+                        attrs.x,
+                        attrs.y,
+                        static_cast<uint32_t>(attrs.width),
+                        static_cast<uint32_t>(attrs.height),
+                    },
+                .handle  = static_cast<uint64_t>(child_windows[i]),
+                .visible = attrs.map_state >= 2,
+            });
         }
 
-        XFree(child_windows);
-        XCloseDisplay(display);
+        ::XFree(child_windows);
+        ::XCloseDisplay(display);
 
         return ret;
     }
