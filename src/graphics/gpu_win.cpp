a9145902c1bc1bbb8c56ac553a5a160b24e21592
ffiirree <ice_qi@163.com>
2023-05-04 13:12:23 +0800

    [windows/graphics]  ignore 'Basic Render Driver'

--- src/graphics/gpu_win.cpp
+++ src/graphics/gpu_win.cpp
@@ -1,41 +1,40 @@
 #ifdef _WIN32
 
-#include "probe/defer.h"
 #include "probe/graphics.h"
 #include "probe/util.h"
 
 #include <DXGI.h>
 #include <Windows.h>
+#include <winrt/base.h>
 
 namespace probe::graphics
 {
     std::vector<gpu_info_t> info()
     {
-        IDXGIFactory *factory = nullptr;
-        if (FAILED(CreateDXGIFactory(__uuidof(IDXGIFactory), (void **)(&factory)))) {
+        winrt::com_ptr<IDXGIFactory1> factory{};
+        if (FAILED(::CreateDXGIFactory1(winrt::guid_of<IDXGIFactory1>(), factory.put_void()))) {
             return {};
         }
-        defer(if (factory != nullptr) {
-            factory->Release();
-            factory = nullptr;
-        });
 
         std::vector<gpu_info_t> cards;
 
-        UINT idx              = 0;
-        IDXGIAdapter *adapter = nullptr;
-        while (factory->EnumAdapters(idx, &adapter) != DXGI_ERROR_NOT_FOUND) {
-            DXGI_ADAPTER_DESC desc{};
-            adapter->GetDesc(&desc);
+        winrt::com_ptr<IDXGIAdapter1> adapter{};
+        for (UINT idx = 0; factory->EnumAdapters1(idx, adapter.put()) != DXGI_ERROR_NOT_FOUND; ++idx) {
+            DXGI_ADAPTER_DESC1 desc{};
+            adapter->GetDesc1(&desc);
+
+            if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) {
+                // Microsoft Basic Render Driver
+                continue;
+            }
 
             cards.emplace_back(gpu_info_t{
-                probe::util::to_utf8(desc.Description), vendor_cast(desc.VendorId),
-                desc.DedicatedVideoMemory, desc.SharedSystemMemory,
-                0 // TODO:
+                .name             = probe::util::to_utf8(desc.Description),
+                .vendor           = vendor_cast(desc.VendorId),
+                .dedicated_memory = desc.DedicatedVideoMemory,
+                .shared_memory    = desc.SharedSystemMemory,
+                .frequency        = 0, // TODO:
             });
-
-            ++idx;
-            adapter->Release();
         }
         return cards;
     }
